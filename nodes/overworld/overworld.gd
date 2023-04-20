9d9e50f18354fefa1cd4e0a8916f3858ec889ae4
Daniel Rodriguez Aires <dannroda@gmail.com>
2023-04-20 17:56:30 -0300

    sky

--- nodes/overworld/overworld.gd
+++ nodes/overworld/overworld.gd
@@ -11,26 +11,20 @@ extends Node3D
 func _ready():
 	player_speed.max_value = player.max_speed
 	player_speed.min_value = player.min_speed
-#	player_speed.step = player.increase_speed_by
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	player_speed.value = player.current_speed
-
 	var player_global = to_global($Player.position)
 	var player_cell = Vector2i(player_global.x,player_global.z)
-#	print(player_global)
 	for cell in cells:
-#		print(cell)
 		var global_cell = to_global($GridMap.map_to_local(cell))
-#		print(global_cell)
 		var to_check = Vector2i(global_cell.x,global_cell.z)
 		if player_cell == to_check:
 			print("same")
 
 func _update_direction(body):
 	print(body)
-#	$Player.change_direction.emit()
 
 
 func _on_player_damage(amount):
