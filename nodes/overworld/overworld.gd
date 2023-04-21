ba32ba60614392d9aaab521c946db24d64a3bd67
Daniel Rodriguez Aires <dannroda@gmail.com>
2023-04-21 00:45:00 -0300

    ui changes

--- nodes/overworld/overworld.gd
+++ nodes/overworld/overworld.gd
@@ -3,10 +3,9 @@ extends Node3D
 @onready var grid_mesh_lib = $GridMap.mesh_library
 @onready var water_pink = grid_mesh_lib.find_item_by_name('Water2')
 @onready var cells = $GridMap.get_used_cells_by_item(water_pink)
-@onready var player_speed = $CanvasLayer/HBoxContainer/VBoxContainer/PlayerSpeed
-@onready var player_life = $CanvasLayer/HBoxContainer/VBoxContainer/PlayerLife
-@onready var player = $Player
-
+@export var player_life:ProgressBar
+@export var player_speed:ProgressBar
+@export var player:CharacterBody3D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player_speed.max_value = player.max_speed
@@ -15,17 +14,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	player_speed.value = player.current_speed
-	var player_global = to_global($Player.position)
+	var player_global = to_global(player.position)
 	var player_cell = Vector2i(player_global.x,player_global.z)
 	for cell in cells:
 		var global_cell = to_global($GridMap.map_to_local(cell))
 		var to_check = Vector2i(global_cell.x,global_cell.z)
 		if player_cell == to_check:
 			print("same")
-
-func _update_direction(body):
-	print(body)
-
-
 func _on_player_damage(amount):
 	player_life.value -= amount
